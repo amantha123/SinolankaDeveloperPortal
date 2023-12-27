c351086799abcb7238ecc044f1d9e78089dcdaa9
amantha1 <amanthag1@gmail.com>
2023-12-27 13:25:28 +0530

    Jenkinsfile

--- src/main/java/com/pack/pages/UserRolesPage.java
+++ src/main/java/com/pack/pages/UserRolesPage.java
@@ -4,14 +4,24 @@ import com.pack.base.TestBase;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
 
 public class UserRolesPage extends TestBase {
 
     @FindBy(xpath = "//span[@class='link-title'][contains(.,'User Roles')]")
     WebElement UserRoles;
 
+    @FindBy(xpath = "//input[contains(@type,'text')]")
+    WebElement Name;
+
     @FindBy(xpath = "//button[@type='submit'][contains(.,'Search')]")
     WebElement SearchBtn;
+
+    @FindBy(xpath = "//span[@title='Admin'][contains(.,'Admin')]")
+    WebElement SearchResult;
+
+    @FindBy(xpath = "//a[contains(@id,'addButton')]")
+    WebElement AddUserRoles;
 
     public UserRolesPage(){
         PageFactory.initElements(driver, this);
@@ -24,5 +34,21 @@ public class UserRolesPage extends TestBase {
     public UserRolesPage clickOnUserRolesPageLink(){
         UserRoles.click();
         return new UserRolesPage();
+    }
+
+    public boolean verifyNameField(){
+        return Name.isDisplayed();
+    }
+
+    public void filterUserRolesPage(String name){
+        UserRoles.click();
+        Name.click();
+        Name.sendKeys(name);
+        SearchBtn.click();
+        SearchResult.isDisplayed();
+    }
+
+    public boolean verifyAddUserRolesBtn(){
+        return AddUserRoles.isDisplayed();
     }
 }
--- src/test/java/com/pack/testcases/UserRolesPageTest.java
+++ src/test/java/com/pack/testcases/UserRolesPageTest.java
@@ -4,9 +4,11 @@ import com.pack.base.TestBase;
 import com.pack.pages.DashboardPage;
 import com.pack.pages.LoginPage;
 import com.pack.pages.UserRolesPage;
+import com.pack.util.TestUtil;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class UserRolesPageTest extends TestBase {
@@ -14,6 +16,8 @@ public class UserRolesPageTest extends TestBase {
     LoginPage loginPage;
     DashboardPage dashboardPage;
     UserRolesPage userRolesPage;
+
+    String sheetName = "User Roles";
 
     public UserRolesPageTest(){
         super();
@@ -36,6 +40,40 @@ public class UserRolesPageTest extends TestBase {
     @Test(priority = 2)
     public void navigateUserRolesPageTest(){
         userRolesPage.clickOnUserRolesPageLink();
+    }
+
+    @Test(priority = 3)
+    public void verifyNameFieldTest(){
+        userRolesPage.clickOnUserRolesPageLink();
+        userRolesPage.verifyNameField();
+    }
+
+    @DataProvider
+    public Object[][] getTestData(){
+        Object data[][] = TestUtil.getTestData(sheetName);
+        return new Object[][] {
+                {"Admin"},
+        };
+    }
+
+    @Test(priority = 4, dataProvider = "getTestData")
+    public void filterUserRolesPageTest(String name){
+        userRolesPage.clickOnUserRolesPageLink();
+        userRolesPage.filterUserRolesPage(name);
+        synchronized (this){
+            try{
+                this.wait(100);
+                System.out.println("Thread in runnable state");
+            }catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Test(priority = 5)
+    public void verifyAddUserRolesBtnTest(){
+        userRolesPage.clickOnUserRolesPageLink();
+        userRolesPage.verifyAddUserRolesBtn();
     }
 
     @AfterMethod
