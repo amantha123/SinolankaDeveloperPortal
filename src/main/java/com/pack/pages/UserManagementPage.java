8b5dd3f5beccdce0301f4361dfd544369878d53b
amantha1 <amanthag1@gmail.com>
2024-01-08 08:49:04 +0530

    Jenkinsfile

--- src/main/java/com/pack/pages/UserManagementPage.java
+++ src/main/java/com/pack/pages/UserManagementPage.java
@@ -10,6 +10,24 @@ public class UserManagementPage extends TestBase {
     @FindBy(xpath = "//span[@class='link-title'][contains(.,'User Management')]")
     WebElement UserManagement;
 
+    @FindBy(xpath = "//h4[@class='mb-3 mb-md-0'][contains(.,'User List')]")
+    WebElement UserListLabel;
+
+    @FindBy(xpath = "//a[@href='/users/add']")
+    WebElement AddUserBtn;
+
+    @FindBy(xpath = "//input[contains(@type,'text')]")
+    WebElement Name;
+
+    @FindBy(xpath = "//input[contains(@mask,'0*')]")
+    WebElement ContactNo;
+
+    @FindBy(xpath = "//input[contains(@type,'email')]")
+    WebElement Email;
+
+    @FindBy(xpath = "//button[@type='submit'][contains(.,'Search')]")
+    WebElement SearchBtn;
+
     public UserManagementPage(){
         PageFactory.initElements(driver, this);
     }
@@ -21,5 +39,31 @@ public class UserManagementPage extends TestBase {
     public UserManagementPage clickOnUserManagementPageLink(){
         UserManagement.click();
         return new UserManagementPage();
+    }
+
+    public boolean verifyUserListLabel(){
+        return UserListLabel.isDisplayed();
+    }
+
+    public boolean verifyAddUserBtn(){
+        return AddUserBtn.isDisplayed();
+    }
+
+    public boolean verifyNameField(){
+        return Name.isDisplayed();
+    }
+
+    public boolean verifyContactNoField(){
+        return ContactNo.isDisplayed();
+    }
+
+    public boolean verifyEmailField(){
+        return Email.isDisplayed();
+    }
+
+    public void verifyFilteringUserListByName(){
+        Name.click();
+        Name.sendKeys("Amantha Gunawardena");
+        SearchBtn.click();
     }
 }
